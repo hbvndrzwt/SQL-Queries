0c530bf591e8f429c9716abdc5a6b82288753cb4
MENDIXDOMAIN\hzw <huub.van.der.zwet@mendix.com>
2018-12-05 11:33:37 +0100

    Update community.Challenges_Progress.sql

--- community.Challenges_Progress.sql
+++ community.Challenges_Progress.sql
@@ -2,17 +2,19 @@
 
 /*
 Questions:
-1. Is it possible for users to receive the same challenge multiple times?
+1. Is it possible for users to receive the same challenge multiple times? - Yes, it is. 
 
 
 
 */
 
+-- 1. Load all Events needed for Challenge Assigning and Completion
 SELECT  *
 INTO #Events
 FROM PlatformAnalytics_PullPush_Platform_Event 
-WHERE EventType IN('ChallengeAssigned', 'ChallengeCompleted', 'ChallengeCreditClaimed', 'ChallengeExpired', 'ChallengeStepCompleted')
+WHERE EventType IN('ChallengeAssigned', 'ChallengeCompleted', 'ChallengeExpired')
 
+-- 2. Link the Correct Challenge Assigned to Challenge Expired events
 SELECT	a.OpenId,
 		a.ChallengeId,
 		a.Status,
@@ -61,7 +63,7 @@ LEFT JOIN
 ON a.LastChallengeEventChangedId = b.ChangedId
 
 
-
+-- 3. Link the Correct Challenge Assigned to Challenge Completed events
 SELECT	a.OpenId,
 		a.ChallengeId,
 		a.Status,
@@ -109,7 +111,7 @@ LEFT JOIN
 )b
 ON a.LastChallengeEventChangedId = b.ChangedId
 
-
+-- 4. Combine all Assigned Challenges that are Completed
 SELECT *
 INTO #ChallengeEnded
 FROM
@@ -120,6 +122,7 @@ FROM
 )a
 ORDER BY OpenId
 
+-- 5. Find all Assigned Challenges that do not have an Ended Activity
 SELECT  a.OpenId,
 		a.ExtraInfo1 AS ChallengeId,
 		'Currently Active' AS Status,
@@ -141,9 +144,9 @@ LEFT JOIN
 ON a.ChangedId = b.LastChallengeEventChangedId
 WHERE b.LastChallengeEventChangedId IS NULL
 
-
+-- 6. Combine all Assigned Challenges
 SELECT *
---INTO #ChallengesMerged
+INTO #ChallengesMerged
 FROM
 (
 	SELECT * FROM #ChallengeEnded
@@ -152,81 +155,14 @@ FROM
 )a
 ORDER BY OpenId
 
+SELECT *
+INTO community.Challenges_Progress
+FROM #ChallengesMerged
 
 
 
 
 
-
-
-/*
-SELECT	a.OpenId,
-		a.ChallengeId,
-		a.Status,
-		b.Timestamp AS TimeChallengeAssigned,
-		a.DateTimeEnded,
-		LastChallengeEventChangedId,
-		c.Timestamp AS TimeCreditClaimed
-INTO #ChallengesCompleted
-FROM
-(
-	SELECT  a.OpenId,
-			a.ChallengeId,
-			a.DateTimeEnded,
-			'Completed' AS Status,
-			MAX(b.ChangedId) AS LastChallengeEventChangedId,
-			MAX(c.ChangedId) AS LastCreditClaimedEventChangedId
-	FROM 
-	(
-		SELECT  OpenId,
-				ExtraInfo1 AS ChallengeId,
-				Timestamp AS DateTimeEnded
-		FROM #Events
-		WHERE EventType IN ('ChallengeCompleted')
-		
-	)a
-	LEFT JOIN
-	(
-		SELECT  ChangedId,
-				OpenId,
-				ExtraInfo1 AS ChallengeId,
-				Timestamp AS DateTimeAssigned
-		FROM #Events
-		WHERE EventType IN ('ChallengeAssigned')
-		
-	)b
-	ON a.OpenId = b.OpenId AND a.ChallengeId = b.ChallengeId AND DateTimeAssigned < DateTimeEnded
-	
-	LEFT JOIN
-	(
-		SELECT  OpenId,
-				ChangedId,
-				ExtraInfo1 AS ChallengeId,
-				Timestamp AS DateTimeCreditClaimed
-		FROM #Events
-		WHERE EventType IN ('ChallengeCreditClaimed')
-	)c
-	ON a.OpenId = c.OpenId AND a.ChallengeId = c.ChallengeId AND DateTimeEnded < DateTimeCreditClaimed
-	GROUP BY	a.OpenId,
-				a.ChallengeId,
-				a.DateTimeEnded
- 
-)a
-LEFT JOIN
-(
-	SELECT *
-	FROM #Events
-	WHERE EventType IN ('ChallengeAssigned')
-)b
-ON a.LastChallengeEventChangedId = b.ChangedId
-LEFT JOIN
-(
-	SELECT *
-	FROM #Events
-	WHERE EventType IN ('ChallengeCreditClaimed')
-)c
-ON a.LastCreditClaimedEventChangedId = c.ChangedId
-*/
 
 
 
